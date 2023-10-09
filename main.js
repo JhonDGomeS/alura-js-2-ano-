1f8591ab5de0296080dfabe19221ead0d81e5be5
Jhonatan Dias Gomes <133127553+JhonDGomeS@users.noreply.github.com>
2023-10-09 12:26:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
 function pom(){
 document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('.tecla_pom').onclick = pom;
-
 function clap(){
 document.querySelector('#som_tecla_clap').play();
 }
-document.querySelector('.tecla_clap').onclick = clap;
-
 function tim(){
 document.querySelector('#som_tecla_tim').play();
 }
-document.querySelector('.tecla_tim').onclick = tim;
-
 function puff(){
 document.querySelector('#som_tecla_puff').play();
 }
-document.querySelector('.tecla_puff').onclick = puff;
-    
-function tim(){
-document.querySelector('#som_tecla_tim').play();
+function splash(){
+document.querySelector('#som_tecla_splash').play();
 }
-document.querySelector('.tecla_tim').onclick = tim;
+function toim(){
+document.querySelector('#som_tecla_toim').play();
+}
+function psh(){
+document.querySelector('#som_tecla_psh').play();
+}
+function tic(){
+document.querySelector('#som_tecla_tic').play();
+}
+function tom(){
+document.querySelector('#som_tecla_tom').play();
+}
+listaDeTeclas[0].onclick = pom;
+listaDeTeclas[1].onclick = clap;
+listaDeTeclas[2].onclick = tim;
+listaDeTeclas[3].onclick = puff;
+listaDeTeclas[4].onclick = splash;
+listaDeTeclas[5].onclick = toim;
+listaDeTeclas[6].onclick = psh;
+listaDeTeclas[7].onclick = tic;
+listaDeTeclas[8].onclick = tom;
